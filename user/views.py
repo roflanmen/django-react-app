579fbabe96eb59493b732b55a0bce35cf4ecba75
Bohdan Navalkovskyi <bohdan7nava@gmail.com>
2024-04-28 18:42:21 +0300

    Get myself endpoint

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -44,3 +44,9 @@ class UserViewSet(ModelViewSet):
             raise PermissionDenied("You do not have permission to edit this user.")
 
         return super().update(request, *args, **kwargs, partial=True)
+
+    @action(methods=["get"], url_path="me", detail=False, permission_classes=[IsAuthenticated])
+    def me(self, request: Request):
+        user = request.user.profile
+        serializer = UserRetrieveSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
