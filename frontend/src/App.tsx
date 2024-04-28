c87d3aca57cbaee1a09dfe10b234cec2071ba376
Bohdan Navalkovskyi <bohdan7nava@gmail.com>
2024-04-28 22:38:14 +0300

    Update component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 
-const Example = (props: {title : string, number: number}) => {
-    return <div>{props.title} {props.number}</div>
+const Example = (props: {title : string}) => {
+    return <div>{props.title}</div>
 }
 
 const App = () => {
-    const [data, setData] = React.useState("");
-    React.useEffect(() => {
-        fetch("/api/test")
-            .then((res) => res.text())
-            .then((data) => setData(data));
-    }, []);
-    return <div>{data}</div>;
+  return <Example title="Needz api"/>
 };
-// const App = () => {
-//   return <Example title="Hello World" number={23}/>
-// };
 
 export default App;
